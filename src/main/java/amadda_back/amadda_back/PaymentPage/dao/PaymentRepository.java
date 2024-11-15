f9d712513364702fcba0557ca5c9b99b890b39e3
cleanstar123 <cleanstar1212@gmail.com>
2024-11-15 18:33:12 +0900

    11/15 wonjun4

--- src/main/java/amadda_back/amadda_back/PaymentPage/dao/PaymentRepository.java
+++ src/main/java/amadda_back/amadda_back/PaymentPage/dao/PaymentRepository.java
@@ -1,13 +1,17 @@
 package amadda_back.amadda_back.PaymentPage.dao;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import amadda_back.amadda_back.PaymentPage.domain.entity.Payment;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, Integer>{
-    Optional<Payment> findByMerchantUid(String merchantUid); // 혹은 다른 검색 조건을 사용합니다.
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+    
+    @Modifying
+    @Query("UPDATE Payment p SET p.status = :status, p.modified = CURRENT_TIMESTAMP WHERE p.id = :paymentId AND p.status = 'pending'")
+    int updatePaymentStatus(@Param("paymentId") Long paymentId, @Param("status") String status);
 }
