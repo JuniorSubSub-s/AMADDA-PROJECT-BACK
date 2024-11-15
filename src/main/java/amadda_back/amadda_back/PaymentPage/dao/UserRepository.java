922815f860533c1a6a26bdda41f22b2e4e2149bf
cleanstar123 <cleanstar1212@gmail.com>
2024-11-15 10:19:44 +0900

    11/15 wonjun2

--- src/main/java/amadda_back/amadda_back/PaymentPage/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package amadda_back.amadda_back.PaymentPage.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import amadda_back.amadda_back.PaymentPage.domain.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
-}
