e427ce292fd7dc8e5e03e842c32dea89f8ad7657
chaeni_beam <twokids1005@naver.com>
2024-11-20 09:47:21 +0900

    1120 채은 오전

--- src/main/java/amadda_back/amadda_back/View/service/PostService.java
+++ src/main/java/amadda_back/amadda_back/View/service/PostService.java
@@ -16,12 +16,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import amadda_back.amadda_back.View.dao.FoodImageDAO;
 import amadda_back.amadda_back.View.dao.PostDAO;
+import amadda_back.amadda_back.View.dao.PurchaseDAO;
 import amadda_back.amadda_back.View.dao.TagDAO;
 import amadda_back.amadda_back.View.dao.ThemeDAO;
 import amadda_back.amadda_back.View.dao.TopicDAO;
 import amadda_back.amadda_back.View.domain.entity.FoodImageEntity;
 import amadda_back.amadda_back.View.domain.entity.PostEntity;
 import amadda_back.amadda_back.View.domain.entity.PostResponseDTO;
+import amadda_back.amadda_back.View.domain.entity.PurchaseEntity;
 import amadda_back.amadda_back.View.domain.entity.RestaurantEntity;
 import amadda_back.amadda_back.View.domain.entity.TagEntity;
 import amadda_back.amadda_back.View.domain.entity.ThemeEntity;
@@ -56,6 +58,9 @@ public class PostService {
 
     @Autowired
     private TopicDAO topicDAO;
+
+    @Autowired
+    private PurchaseDAO purchaseDAO;
 
     // 레스토랑 ID에 해당하는 포스트를 가져오는 메서드
     public List<PostResponseDTO> getPostsByRestaurantId(Integer restaurantId) {
@@ -269,9 +274,14 @@ public class PostService {
 
     }
 
-    // 테마 불러오기
+    // 테마 리스트 불러오기
     public List<ThemeEntity> getAllThemes() {
         return themeDAO.findAll();
+    }
+
+    // 사용자가 구매한 테마 불러오기
+    public List<PurchaseEntity> getPurchasesByUserId(Integer userId) {
+        return purchaseDAO.findByUser_UserId(userId);
     }
 
 }
