2e50db75b6816593a59eceab35576116b7d9066b
cleanstar123 <cleanstar1212@gmail.com>
2024-11-25 16:57:23 +0900

    11/25 원준

--- src/main/java/amadda_back/amadda_back/View/service/PostService.java
+++ src/main/java/amadda_back/amadda_back/View/service/PostService.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import amadda_back.amadda_back.View.dao.FoodImageDAO;
